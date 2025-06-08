ec51749b27a691d789486b879a49fa8e992b1faa
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-06-07 21:12:29 -0300

    refactor: removing try/catch blocks from controllers

--- src/controller/AdopterController.ts
+++ src/controller/AdopterController.ts
@@ -12,145 +12,126 @@ import AddressEntity from "../domain/entities/AddressEntity";
 import ResponseAPI from "../domain/models/ResponseAPI";
 
 export default class AdopterController {
-   constructor(private repository: AdopterRepository) { }
+   constructor(private repository: AdopterRepository) {}
 
    async createAdopter(
-      req: Request<Record<string, string>, {}, CreateAdopterDTO>, 
+      req: Request<Record<string, string>, {}, CreateAdopterDTO>,
       res: Response<ResponseAPI>
-   ) { 
-      try {
-         const { name, email, password, address: addressDTO, photo } = req.body as CreateAdopterDTO;
+   ) {
+      const { name, email, password, address: addressDTO, photo } = req.body as CreateAdopterDTO;
 
-         const address = addressDTO 
-            ? new Address(uuid(), addressDTO.state, addressDTO.city) 
-            : undefined;
+      const address = addressDTO
+         ? new Address(uuid(), addressDTO.state, addressDTO.city)
+         : undefined;
 
-         const adopter = new CreateAdopterDTO(
-            name,
-            email,
-            password,
-            photo,
-            address
-         );
+      const adopter = new CreateAdopterDTO(
+         name,
+         email,
+         password,
+         photo,
+         address
+      );
 
-         const entityCreated = await this.repository.createAdopter(AdopterMapper.toEntity(adopter));
-         if (entityCreated) {
-            const model = AdopterMapper.toModel(entityCreated);
-            res.status(201).json(new ResponseAPI("Adopter created", instanceToPlain(AdopterMapper.toResponse(model))));
-         }
-      } catch (err) {
-         res.status(500).json(new ResponseAPI("Error creating adopter", err));
-         return;
+      const entityCreated = await this.repository.createAdopter(AdopterMapper.toEntity(adopter));
+
+      if (entityCreated) {
+         const model = AdopterMapper.toModel(entityCreated);
+         res.status(201).json(new ResponseAPI("Adopter created", instanceToPlain(AdopterMapper.toResponse(model))));
       }
    }
 
    async getAdopter(
-      req: Request<Record<string, string>, {}, CreateAdopterDTO>, 
+      req: Request<Record<string, string>, {}, CreateAdopterDTO>,
       res: Response<ResponseAPI>
    ) {
-      try {
-         const { id } = req.params;
-         const entity = await this.repository.getAdopter(id);
-         if (entity) {
-            const model = AdopterMapper.toModel(entity);
-            res.status(200).json(new ResponseAPI("Adopter found", instanceToPlain(AdopterMapper.toResponse(model))));
-         }
-         else {
-            res.status(404).json(new ResponseAPI("Adopter not found"));
-         }
-      }
-      catch (err) {
-         res.status(500).json(new ResponseAPI("Error getting adopter", err));
+      const { id } = req.params;
+
+      const entity = await this.repository.getAdopter(id);
+
+      if (entity) {
+         const model = AdopterMapper.toModel(entity);
+         res.status(200).json(new ResponseAPI("Adopter found", instanceToPlain(AdopterMapper.toResponse(model))));
+      } else {
+         res.status(404).json(new ResponseAPI("Adopter not found"));
       }
    }
 
    async getAllAdopters(
-      req: Request<Record<string, string>, {}, CreateAdopterDTO>, 
+      req: Request<Record<string, string>, {}, CreateAdopterDTO>,
       res: Response<ResponseAPI>
    ) {
-      try {
-         const entities = await this.repository.getAllAdopters();
-         const models = entities.map((entity) => AdopterMapper.toModel(entity));
-         res.status(200).json(new ResponseAPI("Adopters found", models.map((model) => instanceToPlain(AdopterMapper.toResponse(model)))));
-      }
-      catch (err) {
-         res.status(500).json(new ResponseAPI("Error getting adopters", err));
-      }
+      const entities = await this.repository.getAllAdopters();
+
+      const models = entities.map((entity) => AdopterMapper.toModel(entity));
+
+      res.status(200).json(
+         new ResponseAPI(
+            "Adopters found",
+            models.map((model) => instanceToPlain(AdopterMapper.toResponse(model)))
+         )
+      );
    }
 
    async updateAdopter(
-      req: Request<Record<string, string>, {}, CreateAdopterDTO>, 
+      req: Request<Record<string, string>, {}, CreateAdopterDTO>,
       res: Response<ResponseAPI>
    ) {
-      try {
-         const { name, email, password, address, photo } = req.body as Adopter;
-         const { id } = req.params;
+      const { name, email, password, address, photo } = req.body as Adopter;
+      const { id } = req.params;
 
-         const adopter = new Adopter(
-            id,
-            name,
-            email,
-            password,
-            photo ?? undefined,
-            address ?? undefined
-         );
+      const adopter = new Adopter(
+         id,
+         name,
+         email,
+         password,
+         photo ?? undefined,
+         address ?? undefined
+      );
 
-         const entityUpdated = await this.repository.updateAdopter(id, AdopterMapper.toEntity(adopter) as AdopterEntity);
-         if (entityUpdated) {
-            const model = AdopterMapper.toModel(entityUpdated);
-            res.status(200).json(new ResponseAPI("Adopter updated", instanceToPlain(AdopterMapper.toResponse(model))));
-         }
-         else {
-            res.status(404).json(new ResponseAPI("Adopter not found"));
-         }
-      }
-      catch (err) {
-         res.status(500).json(new ResponseAPI("Error updating adopter", err));
+      const entityUpdated = await this.repository.updateAdopter(
+         id,
+         AdopterMapper.toEntity(adopter) as AdopterEntity
+      );
+
+      if (entityUpdated) {
+         const model = AdopterMapper.toModel(entityUpdated);
+         res.status(200).json(new ResponseAPI("Adopter updated", instanceToPlain(AdopterMapper.toResponse(model))));
+      } else {
+         res.status(404).json(new ResponseAPI("Adopter not found"));
       }
    }
 
    async updateAddress(
-      req: Request<Record<string, string>, {}, CreateAddressDTO>, 
+      req: Request<Record<string, string>, {}, CreateAddressDTO>,
       res: Response<ResponseAPI>
    ) {
-      try {
-         const { city, state } = req.body as CreateAddressDTO;
-         const { id } = req.params;
+      const { city, state } = req.body as CreateAddressDTO;
+      const { id } = req.params;
 
-         const address = new AddressEntity(state, city);
+      const address = new AddressEntity(state, city);
 
-         const entityUpdated = await this.repository.updateAddress(id, address);
-         if (entityUpdated) {
-            const model = AdopterMapper.toModel(entityUpdated);
-            res.status(200).json(new ResponseAPI("Adopter address updated", instanceToPlain(AdopterMapper.toResponse(model))));
-         }
-         else {
-            res.status(404).json(new ResponseAPI("Adopter not found"));
-         }
-      }
-      catch (err) {
-         res.status(500).json(new ResponseAPI("Error updating adopter address", err));
+      const entityUpdated = await this.repository.updateAddress(id, address);
+
+      if (entityUpdated) {
+         const model = AdopterMapper.toModel(entityUpdated);
+         res.status(200).json(new ResponseAPI("Adopter address updated", instanceToPlain(AdopterMapper.toResponse(model))));
+      } else {
+         res.status(404).json(new ResponseAPI("Adopter not found"));
       }
    }
 
    async deleteAdopter(
-      req: Request<Record<string, string>, {}, CreateAdopterDTO>, 
+      req: Request<Record<string, string>, {}, CreateAdopterDTO>,
       res: Response<ResponseAPI>
    ) {
-      try {
-         const { id } = req.params;
+      const { id } = req.params;
 
-         const adopterDeleted = await this.repository.deleteAdopter(id);
-         if (adopterDeleted) {
-            res.status(200).json(new ResponseAPI("Adopter deleted"));
-         }
-         else {
-            res.status(404).json(new ResponseAPI("Adopter not found"));
-         }
-      }
-      catch (err) {
-         res.status(500).json(new ResponseAPI("Error deleting adopter", err));
+      const adopterDeleted = await this.repository.deleteAdopter(id);
+
+      if (adopterDeleted) {
+         res.status(200).json(new ResponseAPI("Adopter deleted"));
+      } else {
+         res.status(404).json(new ResponseAPI("Adopter not found"));
       }
    }
 }
-
--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -15,38 +15,38 @@ export default class PetController {
   constructor(private repository: PetRepostiory) {}
 
   async createPet(
-    req: Request<Record<string, string>, {}, CreatePetDTO>, 
+    req: Request<Record<string, string>, {}, CreatePetDTO>,
     res: Response<ResponseAPI>
   ): Promise<void> {
-    try {
+    const { name, species, birthDate, sex, size } = req.body;
 
-      const { name, species, birthDate, sex, size } = req.body;
+    const pet = new CreatePetDTO(
+      name,
+      species,
+      birthDate,
+      sex,
+      size
+    );
 
-      const pet = new CreatePetDTO(
-        name, 
-        species, 
-        birthDate, 
-        sex, 
-        size
+    const entityCreated = await this.repository.createPet(
+      PetMapper.toEntity(pet)
+    );
+
+    if (entityCreated) {
+      const model = PetMapper.toModel(entityCreated);
+      res.status(201).json(
+        new ResponseAPI(
+          "Pet created",
+          instanceToPlain(PetMapper.toResponse(model))
+        )
       );
-
-      const entityCreated = await this.repository.createPet(
-        PetMapper.toEntity(pet)
-      );
-      if (entityCreated) {
-        const model = PetMapper.toModel(entityCreated);
-        res.status(201).json(new ResponseAPI("Pet created", instanceToPlain(PetMapper.toResponse(model))));
-      }
-    } catch (err) {
-      res.status(500).json(new ResponseAPI("Error creating pet", err));
     }
   }
 
- async createPetsBatch(
-  req: Request<Record<string, string>, {}, Array<CreatePetDTO>>, 
-  res: Response<ResponseAPI>
-): Promise<void> {
-  try {
+  async createPetsBatch(
+    req: Request<Record<string, string>, {}, Array<CreatePetDTO>>,
+    res: Response<ResponseAPI>
+  ): Promise<void> {
     const petsToCreate = req.body;
 
     if (!petsToCreate.length) {
@@ -62,133 +62,133 @@ export default class PetController {
       const model = PetMapper.toModel(entitiesCreated);
 
       res.status(201).json(
-          new ResponseAPI(
-            "Pets created",
-            instanceToPlain(PetMapper.toResponse(model))
-          )
-        );
+        new ResponseAPI(
+          "Pets created",
+          instanceToPlain(PetMapper.toResponse(model))
+        )
+      );
     }
-  } catch (err) {
-    res.status(500).json(new ResponseAPI("Error creating pets", err));
   }
-}
-
 
   async getPet(
-    req: Request<Record<string, string>, {}, {}>, 
+    req: Request<Record<string, string>, {}, {}>,
     res: Response<ResponseAPI>
   ): Promise<void> {
-    try {
-      const { id } = req.params;
-      const entity = await this.repository.getPet(id);
-      if (entity) {
-        const model = PetMapper.toModel(entity);
-        res.status(200).json(new ResponseAPI("Pet found", instanceToPlain(PetMapper.toResponse(model))));
-      } else {
-        res.status(404).json(new ResponseAPI("Pet not found"));
-      }
-    } catch (err) {
-      res.status(500).json(new ResponseAPI("Error getting pet"));
+    const { id } = req.params;
+
+    const entity = await this.repository.getPet(id);
+
+    if (entity) {
+      const model = PetMapper.toModel(entity);
+      res.status(200).json(
+        new ResponseAPI(
+          "Pet found",
+          instanceToPlain(PetMapper.toResponse(model))
+        )
+      );
+    } else {
+      res.status(404).json(new ResponseAPI("Pet not found"));
     }
   }
 
   async getAllPets(
-    req: Request<Record<string, string>, {}, {}, PetFilters>, 
+    req: Request<Record<string, string>, {}, {}, PetFilters>,
     res: Response<ResponseAPI>
   ): Promise<void> {
-    try {
-      const filters = req.query;
-      const pets = await this.repository.getAllPets(filters);
+    const filters = req.query;
+    const pets = await this.repository.getAllPets(filters);
 
-      if (pets.length == 0) {
-        res.status(204).json(new ResponseAPI("No pets found"));
-        return;
-      }
-
-      res.status(200).json(new ResponseAPI("Pets found", pets.map((pet) => {
-        const model = PetMapper.toModel(pet);
-        return instanceToPlain(PetMapper.toResponse(model));
-      })));
-    } catch (err) {
-      res.status(500).json(new ResponseAPI("Error getting pets"));
+    if (pets.length == 0) {
+      res.status(204).json(new ResponseAPI("No pets found"));
+      return;
     }
+
+    res.status(200).json(
+      new ResponseAPI(
+        "Pets found",
+        pets.map((pet) => {
+          const model = PetMapper.toModel(pet);
+          return instanceToPlain(PetMapper.toResponse(model));
+        })
+      )
+    );
   }
 
   async updatePet(
-    req: Request<Record<string, string>, {}, CreatePetDTO>, 
+    req: Request<Record<string, string>, {}, CreatePetDTO>,
     res: Response<ResponseAPI>
   ): Promise<void> {
-    try {
-      const { name, species, birthDate, sex, size } = req.body;
-      const { id } = req.params;
+    const { name, species, birthDate, sex, size } = req.body;
+    const { id } = req.params;
 
-      if (
-        !isValidEnumValue(EnumSpecies, species) &&
-        !isValidEnumValue(EnumPetSex, sex)
-      ) {
-        res.status(400).json(new ResponseAPI("Invalid species or sex"));
-        return;
-      }
+    if (
+      !isValidEnumValue(EnumSpecies, species) &&
+      !isValidEnumValue(EnumPetSex, sex)
+    ) {
+      res.status(400).json(new ResponseAPI("Invalid species or sex"));
+      return;
+    }
 
-      const pet = new Pet(
-        id,
-        name,
-        species,
-        birthDate,
-        sex,
-        size
+    const pet = new Pet(
+      id,
+      name,
+      species,
+      birthDate,
+      sex,
+      size
+    );
+
+    const entityUpdated = await this.repository.updatePet(
+      id,
+      PetMapper.toEntity(pet) as PetEntity
+    );
+
+    if (entityUpdated) {
+      const model = PetMapper.toModel(entityUpdated);
+      res.status(200).json(
+        new ResponseAPI(
+          "Pet updated",
+          instanceToPlain(PetMapper.toResponse(model))
+        )
       );
-
-      const entityUpdated = await this.repository.updatePet(
-        id,
-        PetMapper.toEntity(pet) as PetEntity
-      );
-      if (entityUpdated) {
-        const model = PetMapper.toModel(entityUpdated);
-        res.status(200).json(new ResponseAPI("Pet updated", instanceToPlain(PetMapper.toResponse(model))));
-      } else {
-        res.status(404).json(new ResponseAPI("Pet not found"));
-      }
-    } catch (err) {
-      res.status(500).json(new ResponseAPI("Error updating pet"));
+    } else {
+      res.status(404).json(new ResponseAPI("Pet not found"));
     }
   }
 
   async deletePet(
-    req: Request<Record<string, string>, {}, {}>, 
+    req: Request<Record<string, string>, {}, {}>,
     res: Response<ResponseAPI>
   ): Promise<void> {
-    try {
-      const { id } = req.params;
+    const { id } = req.params;
 
-      const petDeleted = await this.repository.deletePet(id);
-      if (petDeleted) {
-        res.status(200).json(new ResponseAPI("Pet deleted"));
-      } else {
-        res.status(404).json(new ResponseAPI("Pet not found"));
-      }
-    } catch (err) {
-      res.status(500).json(new ResponseAPI("Error deleting pet"));
+    const petDeleted = await this.repository.deletePet(id);
+
+    if (petDeleted) {
+      res.status(200).json(new ResponseAPI("Pet deleted"));
+    } else {
+      res.status(404).json(new ResponseAPI("Pet not found"));
     }
   }
 
   async adoptPet(
-    req: Request<Record<string, string>, {}, {}>, 
+    req: Request<Record<string, string>, {}, {}>,
     res: Response<ResponseAPI>
   ): Promise<void> {
-    try {
-      const { petId, adopterId } = req.params;
-      const entity = await this.repository.adoptPet(petId, adopterId);
+    const { petId, adopterId } = req.params;
 
-      if (entity) {
-        const model = PetMapper.toModel(entity);
-        res.status(200).json(new ResponseAPI("Pet adopted", instanceToPlain(PetMapper.toResponse(model))));
-      } else {
-        res.status(404).json(new ResponseAPI("Pet not found"));
-      }
-    } catch (err) {
-      res.status(500).json(new ResponseAPI("Error adopting pet"));
+    const entity = await this.repository.adoptPet(petId, adopterId);
+
+    if (entity) {
+      const model = PetMapper.toModel(entity);
+      res.status(200).json(
+        new ResponseAPI(
+          "Pet adopted",
+          instanceToPlain(PetMapper.toResponse(model))
+        )
+      );
+    } else {
+      res.status(404).json(new ResponseAPI("Pet not found"));
     }
   }
 }
-
