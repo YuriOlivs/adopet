a445e452513afc187490d10a0900c7dab9806527
Yuri Oliveira <yuri.os.2004@outlook.com>
2025-06-10 11:04:32 -0300

    feat: adding shelter entity

---
+++ src/domain/models/Shelter/CreateShelterDTO.ts
@@ -0,0 +1,13 @@
+export default class CreateShelterDTO {
+   name: string;
+   password: string;
+   email: string;
+   phone: string;
+
+   constructor(name: string, password: string, email: string, phone: string) {
+      this.name = name;
+      this.password = password;
+      this.email = email;
+      this.phone = phone;
+   }
+}
